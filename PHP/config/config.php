fa446bc83492482f23b6da395652cc959a65906b
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-12-12 18:01:53 +0100

    Création du fichier config/config.php

---
+++ PHP/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'TouchePasAuKlaxon',
+        'user' => 'tpak',
+        'pass' => 'Tpak2025!',
+        'charset' => 'utf8mb4'
+    ],
+    'app' => [
+        'base_url' => '/PHP/',
+    ]
+];

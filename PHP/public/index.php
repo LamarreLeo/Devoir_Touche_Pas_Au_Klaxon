b34c5bcb0cdd1238002ab2c9b39bcfaa7f771c91
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-12-12 19:27:28 +0100

    Ajout de la configuration et de la connexion a la base de données dans public/index.php

--- PHP/public/index.php
+++ PHP/public/index.php
@@ -3,11 +3,19 @@ declare(strict_types=1);
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+// Configuration de la base de données
+use Database\Database;
+
+$config = require __DIR__ . "/../config/config.php";
+
+$db = new Database($config);
+$pdo = $db->getPDO();
+
+// Configuration du routeur
 use Buki\Router\Router;
 
 session_start();
 
-// Router
 $router = new Router([
     'base_folder' => '/Devoir_Touche_Pas_Au_Klaxon/PHP/public'
 ]);

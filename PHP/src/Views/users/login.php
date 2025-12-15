dfbec0cd7fef8b13a3156892ec3582198c701258
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-12-15 17:30:07 +0100

    Amélioration du formulaire de connexion

--- PHP/src/Views/users/login.php
+++ PHP/src/Views/users/login.php
@@ -1,17 +1,17 @@
-<div>
+<div class="bg-[#f1f8fc]">
     <div class="flex flex-col items-center justify-center h-[calc(100vh-128px)]">
-        <div class="bg-[#0074C7] text-white w-[400px] p-6 rounded-lg">
+        <div class="bg-[#384050] text-white w-[400px] p-12 rounded-lg">
             <?php if (!empty($error)) : ?>
                 <div class="bg-red-500 text-white p-4 rounded-lg mb-4">
                     <?= htmlspecialchars($error) ?>
                 </div>
             <?php endif ?>
-            <form action="/Devoir_Touche_Pas_Au_Klaxon/PHP/public/login" method="post" class="flex flex-col gap-10">
+            <form action="/Devoir_Touche_Pas_Au_Klaxon/PHP/public/login" method="post" class="flex flex-col gap-6">
                 <div class="flex flex-col gap-6">
-                    <div class="flex flex-col gap-4">
-                        <label for="email">E-mail :</label>
+                    <h3 class="text-2xl font-bold">Connexion</h3>
+                    <div class="flex flex-col gap-6">
                         <input 
-                            class="bg-[#F1F8FC] p-2 rounded-lg focus:outline-none text-[#384050]"
+                            class="bg-[#F1F8FC] p-2 rounded-lg focus:outline-none text-[#384050] h-12"
                             type="email" 
                             name="email" 
                             id="email" 
@@ -19,12 +19,8 @@
                             placeholder="Votre e-mail"
                             required
                         >
-                    </div>
-
-                    <div class="flex flex-col gap-4">
-                        <label for="password">Mot de passe :</label>
                         <input 
-                            class="bg-[#F1F8FC] p-2 rounded-lg focus:outline-none text-[#384050]"
+                            class="bg-[#F1F8FC] p-2 rounded-lg focus:outline-none text-[#384050] h-12"
                             type="password" 
                             name="password" 
                             id="password" 
@@ -33,7 +29,7 @@
                         >
                     </div>
                 </div>
-                <button type="submit" class="bg-[#82b864] p-2 rounded-lg">Se connecter</button>
+                <button type="submit" class="bg-[#82b864] p-2 rounded-lg h-12">Se connecter</button>
             </form>
         </div>
     </div>
